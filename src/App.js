12f1492dcb2144f20a65f622610438f351f005d8
Absher K <absheroamac@gmail.com>
2024-11-24 00:48:15 +0530

    Completed Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const dictiory = [
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+    { word: "Component", meaning: "A reusable building block in React." },
+    { word: "State", meaning: "An object that stores data for a component." },
+  ];
+
+  const [result, setResult] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = () => {
+    const tempResult = dictiory.find(
+      (item) => item.word.toLowerCase() === searchValue.toLowerCase()
+    );
+    if (tempResult) {
+      setResult(tempResult.meaning);
+    } else {
+      setResult("Word not found in the dictionary.");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dictionary App</h1>
+      <input
+        type="text"
+        placeholder="Search for a word..."
+        onChange={(event) => setSearchValue(event.target.value)}
+      ></input>
+      <button onClick={handleSearch}>Search</button>
+      <h4>Definition:</h4>
+      <br />
+      <p>{result}</p>
     </div>
   );
 }
